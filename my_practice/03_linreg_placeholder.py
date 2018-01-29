a91a2b8a47656053d8cf6a81d1c79ad58a0e5938
Pamin Rangsikunpum <pamin2222@hotmail.com>
2018-01-29 14:57:30 +0700

    03 logreg_dataset

---
+++ my_practice/03_linreg_placeholder.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+
+from my_practice import utils
+
+# Define parameters for
